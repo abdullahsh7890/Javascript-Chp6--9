35770a482425472aa0cb652dcad59015ca71495c
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-03-10 01:36:48 +0500

    Add files via upload

---
+++ Javascript Chp 6-9/app.js
@@ -0,0 +1,91 @@
+// Q1
+var a = 5
+document.write("The value of a is : " + a )
+++a;
+document.write(" <br/> The value of ++a is : " + a)
+document.write(" <br/>Now the value of a is : " + a)
+a++;
+document.write("<br/> The value of a++ is : " + a)
+document.write("<br/> Now the value of a is : " + a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+// Q2
+var a = 2;
+var b = 1;
+
+var stage1 = --a;
+document.write("<br/><br/><br/>The output of --a is : " + stage1);
+var stage2 = --a - --b;
+document.write("<br/><br/><br/> The output of  --a --b is : " + stage2);
+var stage3 = --a - --b + ++b ;
+document.write("<br/><br/><br/>The output of --a - --b + ++b is : " + stage3 );
+var stage4 = --a - --b + ++b + b--;
+document.write("<br/><br/><br/> The output of --a - --b + ++b + b-- is : " + stage4);
+var result = --a - --b +  ++b + b--;
+document.write(" <br/> <br/> <br/> The Result is : " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q3
+prompt("Enter Your Full Name")
+
+// Q4
+prompt("Enter a number")
+document.write("<br/><br/><br/>Table of 5")
+var a = 5
+
+document.write("<br/>5x1 =" + a*1)
+document.write("<br/>5x2 =" + a*2)
+document.write("<br/>5x3 =" + a*3)
+document.write("<br/>5x4 =" + a*4)
+document.write("<br/>5x5 =" + a*5)
+document.write("<br/>5x6 =" + a*6)
+document.write("<br/>5x7 =" + a*7)
+document.write("<br/>5x8 =" + a*8)
+document.write("<br/>5x9 =" + a*9)
+document.write("<br/>5x10=" + a*10)
+
+
+
+//   I'm not doing Q6 because I didn't learn to write html in js.
+
+
+
+
+
